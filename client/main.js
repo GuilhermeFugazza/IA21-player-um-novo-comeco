300929233f2d23dc3258682d887991636dda6065
GuilhermeFugazza <104568676+GuilhermeFugazza@users.noreply.github.com>
2023-04-11 14:59:35 +0000

    js update

--- client/main.js
+++ client/main.js
@@ -1,7 +1,16 @@
 
 const containers = document.querySelectorAll("div.ia21-player")
         containers.forEach(el => {
-            const play = el.querySelector("button.play")
+            const play = el.querySelector("button.playpause")
             const video = el.querySelector("video")
+            const timeLine = el.querySelector("dragbar.timeline")
+            const timeLineDrag = timeLine.querySelector(".draggable")
+            const timer = el.querySelector(".timer")
             play.onclick = () => video.play()
+
+            video.addEventListener("timeupdate", () => {
+                const percent = (video.currentTime / video.duration) * 100
+                timeLineDrag.style.setProperty("--percent", `${percent}$`)
+                timer.innerText = 
+             })
         })  
