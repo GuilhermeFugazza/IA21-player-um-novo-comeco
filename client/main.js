e0da137b8be647c139ee974cd581bee8998763e3
Guilherme Fugazza <104568676+GuilhermeFugazza@users.noreply.github.com>
2023-04-18 11:54:18 -0300

    Quadro

--- client/main.js
+++ client/main.js
@@ -1,29 +1,25 @@
-
 const containers = document.querySelectorAll("div.ia21-player")
-containers.forEach(el => {
-    const play = el.querySelector("button.playpause")
-    const video = el.querySelector("video")
-    const timeLine = el.querySelector(".dragbar.timeline")
-    const timeLineDrag = timeLine.querySelector(".draggable")
-    const timer = el.querySelector(".timer")
-    play.onclick = () => video.play()
-    
-    video.currentTime = 58
-    
-    video.addEventListener("timeupdate", () => {
-        const percent = (video.currentTime / video.duration) * 100
-        timeLineDrag.style.setProperty("--percent", `${percent}$`)
-        
-        const s = Math.floor(video.currentTime)
-        const m = Math.floor(s / 60)
-        const h = Math.floor(m / 60)
-        
-        timer.innerText = `${h}:${m%60}:${s%60} `
 
-        //const s = Math.floor(video.currentTime)
-        //const m = Math.floor(s / 60)
-        //const h = Math.floor(m / 60)
-        
-        //timer.innerText = `${h}:${m%60}:${s%60} `
-    })
-})  
+        containers.forEach(el => {
+            const play = el.querySelector("button.playpause")
+            const video = el.querySelector("video")
+            const timeline = el.querySelector(".dragbar.timeline")
+            const timeLineDrag = timeline.querySelector(".draggable")
+            const timer = el.querySelector(".timer")
+            play.onclick = () => video.play()
+
+            //SOCORRO
+            video.currentTime = 59
+
+            video.addEventListener("timeupdate", () => {
+                const percent = (video.currentTime / video.duration) * 100
+                timeLineDrag.style.setProperty("--porcent",`${percent}%`)
+                timer.innerText = Math.floor(video.currentTime)
+                
+                if (timer.innerText >= 60) {
+                    min = Math.floor(timer.innerText/60)
+                    seg = Math.floor(timer.innerText%60)
+                    timer.innerText = min + ":" + seg
+                }
+            })
+        })
